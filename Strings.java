9c1e624df1db485c345ff87e08aae44f2fb0a129
rksorry7255tu <rk7255053708@gmail.com>
2023-10-31 10:13:01 +0530

    Simple String program

---
+++ Strings.java
@@ -0,0 +1,11 @@
+class Strings{
+    public static void main(String[]args){
+        //String declaration
+        String name="Ravi ";
+        String fullname="Ravi kumar";
+        String sentence="My name is Ravi kumar";
+        System.out.println(name);
+        System.out.println(fullname);
+        System.out.println(sentence);
+    }
+}
